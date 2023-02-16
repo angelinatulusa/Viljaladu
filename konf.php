28f42cf1a69903091dae90e64b92e8e6700fb995
angelinatulusa <93324341+angelinatulusa@users.noreply.github.com>
2023-02-16 13:46:39 +0200

    Update konf.php

--- konf.php
+++ konf.php
@@ -1,14 +1,14 @@
 <?php
-/*$baasiaadress = "localhost";
-$baasikasutaja = "tulusa";
-$baasiparool = "123456";
-$baasinimi = "tulusa";
+/*$baasiaadress = "";
+$baasikasutaja = "";
+$baasiparool = "";
+$baasinimi = "";
 $yhendus = new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi);*/
 
-$baasiaadress="d113375.mysql.zonevs.eu";
-$baasikasutaja="d113375_eksam";
-$baasiparool="jalg123456";
-$baasinimi="d113375_eksam";
+$baasiaadress="";
+$baasikasutaja="";
+$baasiparool="";
+$baasinimi="";
 $yhendus=new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi);
 //create table viljaladu( id int not null Primary key AUTO_INCREMENT, autonr varchar(6), sisenemismassi decimal(5,2) DEFAULT -1, lahkumismass decimal(5,2)DEFAULT -1,erinevus decimal(5,2) );
 ?>
